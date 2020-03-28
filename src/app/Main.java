613a6ddf8a84ce8c557aa85faf53db7750a7462e
NimaAB <nimabewrani98@gmail.com>
2020-03-28 10:51:56 +0100

    package app

---
+++ src/app/Main.java
@@ -0,0 +1,16 @@
+package app;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        // code for the first stage impliments her
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
