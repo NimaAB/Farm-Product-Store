d1b21e75c08190a50cb5d543042a362436f9699a
NimaAB <nimabewrani98@gmail.com>
2020-04-02 23:32:17 +0200

    har skrevet Items Class og en Enum for kategoriene, men enumen har ikke riktige verdier enda.

--- src/dataModels/Items.java
+++ src/dataModels/Items.java
@@ -1,4 +1,72 @@
 package dataModels;
 
-public class Items {
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+
+public class Items implements Serializable {
+    private SimpleIntegerProperty artikkelnr;
+    private SimpleStringProperty artikkelNavn;
+    private SimpleStringProperty spesifikasjoner;
+    private SimpleStringProperty kategori;
+
+    public Items(int artikkelnr, String artikkelNavn,String kategori
+            ,String spesifikasjoner){
+        this.artikkelnr = new SimpleIntegerProperty(artikkelnr);
+        this.artikkelNavn= new SimpleStringProperty(artikkelNavn);
+        this.kategori = new SimpleStringProperty(kategori);
+        this.spesifikasjoner = new SimpleStringProperty(spesifikasjoner);
+
+    }
+    public void setArtikkelnr(int artikkelnr){
+        this.artikkelnr.set(artikkelnr+1);
+    }
+    public int getArtikkelnr(){
+        return this.artikkelnr.getValue();
+    }
+
+    public void setArtikkelNavn(String name){
+        this.artikkelNavn.set(name);
+    }
+    public String getArtikkelNavn(){
+        return this.artikkelNavn.getValue();
+    }
+
+    public void setKategori(String kategori){
+        this.kategori.set(kategori);
+    }
+    public String getKategori(){
+        return this.kategori.getValue();
+    }
+
+    public void setSpesifikasjoner(String spesifikasjoner) {
+        this.spesifikasjoner.set(spesifikasjoner);
+    }
+    public String getSpesifikasjoner() {
+        return spesifikasjoner.getValue();
+    }
+
+    //Serielizing part:
+    private void writeObject(ObjectOutputStream st) throws IOException{
+        st.defaultWriteObject();
+        st.writeInt(getArtikkelnr());
+        st.writeUTF(getArtikkelNavn());
+        st.writeUTF(getKategori());
+        st.writeUTF(getSpesifikasjoner());
+    }
+    private void readObject(ObjectInputStream st) throws IOException,ClassNotFoundException{
+        int artikkelnr = st.readInt();
+        String artikkelNavn = st.readUTF();
+        String kategori = st.readUTF();
+        String spesifikasjoner = st.readUTF();
+        this.artikkelnr = new SimpleIntegerProperty(artikkelnr);
+        this.artikkelNavn = new SimpleStringProperty(artikkelNavn);
+        this.kategori = new SimpleStringProperty(kategori);
+        this.spesifikasjoner = new SimpleStringProperty(spesifikasjoner);
+    }
+
 }
