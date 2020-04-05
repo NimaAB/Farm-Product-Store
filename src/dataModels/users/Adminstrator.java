9d3d2d91391933a5b0241b19bbfd11983ee16dc3
NimaAB <nimabewrani98@gmail.com>
2020-04-05 23:18:05 +0200

    har lagt to enkle modeller for Costomer og Adminstrator

--- src/dataModels/users/Adminstrator.java
+++ src/dataModels/users/Adminstrator.java
@@ -1,6 +1,30 @@
 package dataModels.users;
 
+import app.controllers.AdminController;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Adminstrator implements Serializable {
+    private String userName;
+    private String password;
+
+    public Adminstrator(String uName, String password){
+        this.userName = uName;
+        this.password = password;
+    }
+    public void setUserName(String userName){
+        this.userName = userName;
+    }
+    public String getUserName(){
+        return userName;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getPassword(){
+        return password;
+    }
 }
--- src/dataModels/users/Costumer.java
+++ src/dataModels/users/Costumer.java
@@ -1,6 +1,41 @@
 package dataModels.users;
 
+import javafx.beans.property.SimpleIntegerProperty;
+import javafx.beans.property.SimpleStringProperty;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
 
 public class Costumer implements Serializable {
+    private SimpleStringProperty userName;
+    //private SimpleIntegerProperty amount;
+    private SimpleStringProperty password;
+
+    public Costumer(String userName,String password){
+        this.userName = new SimpleStringProperty(userName);
+        this.password = new SimpleStringProperty(password);
+    }
+    public void setUserName(String userName){
+        this.userName.set(userName);
+    }
+    public String getUserName(){
+        return userName.getValue();
+    }
+    public void setPassword(String password){
+        this.password.set(password);
+    }
+    public String getPassword(){
+        return password.getValue();
+    }
+
+    private void writeObject(ObjectOutputStream st) throws IOException{
+        st.defaultWriteObject();
+        st.writeUTF(getUserName());
+    }
+    private void readObject(ObjectInputStream st) throws IOException,ClassNotFoundException{
+        String userName = st.readUTF();
+        this.userName = new SimpleStringProperty(userName);
+    }
 }
