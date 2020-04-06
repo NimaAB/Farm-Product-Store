762904be094d469b582abd015975739933b65f6a
HassanMS1 <60139823+HassanMS1@users.noreply.github.com>
2020-04-06 16:27:30 +0200

    Liten endering, i main filen.

--- src/app/Main.java
+++ src/app/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception {
         Parent root = FXMLLoader.load(getClass().getResource("views/loginView.fxml"));
         primaryStage.setTitle("Login");
-        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.setScene(new Scene(root));
         primaryStage.show();
     }
 
