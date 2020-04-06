978193bab40dbb44a262a17305e8874a983414eb
Glaysa <s344047@oslomet.no>
2020-04-06 16:21:34 +0200

    start

--- src/app/Main.java
+++ src/app/Main.java
@@ -1,13 +1,19 @@
 package app;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // code for the first stage impliments her
+        Parent root = FXMLLoader.load(getClass().getResource("views/loginView.fxml"));
+        primaryStage.setTitle("Login");
+        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.show();
     }
 
     public static void main(String[] args) {
