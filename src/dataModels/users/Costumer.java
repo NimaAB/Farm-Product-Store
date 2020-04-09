e73557dc2fb5f7b11e0af5e65d05ada196e43928
NimaAB <nimabewrani98@gmail.com>
2020-04-09 23:15:32 +0200

    redigerte <Costumer.java>

--- src/dataModels/users/Costumer.java
+++ src/dataModels/users/Costumer.java
@@ -1,5 +1,6 @@
 package dataModels.users;
 
+import dataModels.Items;
 import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 
@@ -7,35 +8,36 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.ArrayList;
 
 public class Costumer implements Serializable {
-    private SimpleStringProperty userName;
-    //private SimpleIntegerProperty amount;
-    private SimpleStringProperty password;
+    private String userName;
+    private String password;
+    private ArrayList<Items> configurationList;
 
-    public Costumer(String userName,String password){
-        this.userName = new SimpleStringProperty(userName);
-        this.password = new SimpleStringProperty(password);
+    public Costumer(String userName,String password,ArrayList<Items> configurationList){
+       this.userName = userName;
+       this.password = password;
+       this.configurationList = configurationList;
     }
-    public void setUserName(String userName){
-        this.userName.set(userName);
+
+    public void setUserName(String userName) {
+        this.userName = userName;
     }
     public String getUserName(){
-        return userName.getValue();
+        return userName;
     }
     public void setPassword(String password){
-        this.password.set(password);
+        this.password = password;
     }
     public String getPassword(){
-        return password.getValue();
+        return password;
     }
 
-    private void writeObject(ObjectOutputStream st) throws IOException{
-        st.defaultWriteObject();
-        st.writeUTF(getUserName());
+    public void setConfigurationList(ArrayList<Items> configurationList){
+        this.configurationList=configurationList;
     }
-    private void readObject(ObjectInputStream st) throws IOException,ClassNotFoundException{
-        String userName = st.readUTF();
-        this.userName = new SimpleStringProperty(userName);
+    public ArrayList<Items> getConfigurationList() {
+        return configurationList;
     }
 }
