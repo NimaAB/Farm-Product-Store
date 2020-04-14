a04cef9124c1e3ec27dfe4557d6d817e316f7b6c
NimaAB <nimabewrani98@gmail.com>
2020-04-14 15:51:34 +0200

    Configuration løsning

--- src/app/controllers/CostumerController.java
+++ src/app/controllers/CostumerController.java
@@ -1,14 +1,26 @@
 package app.controllers;
 
 import app.Load;
+import dataModels.data.Components;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
 
+import java.util.ArrayList;
+
 public class CostumerController {
     @FXML
     BorderPane customerPane;
+    //ArrayList av configuration:
+
+    private double totalPrice(ArrayList<Components> configurationList){
+        double totalPris = 0;
+        for(Components item:configurationList){
+            totalPris += item.getComponentPrice();
+        }
+        return totalPris;
+    }
     //vi trenger et sted for å legge inn komponeneter: konfiguration list.
 
     @FXML
