e9b1553f00edbcac86dc4d8808836bd6b9e9e88f
Glaysa <s344047@oslomet.no>
2020-04-23 21:21:18 +0200

    Validations oppdatert

--- src/validations/Validations.java
+++ src/validations/Validations.java
@@ -9,26 +9,35 @@ public class Validations {
     private transient String componentSpecs;
     private transient double componentPrice;
 
+    private String replaceComma(String toValidate){
+        String comma = ",";
+        if(toValidate.contains(comma)){
+            toValidate = toValidate.replace(',','-');
+        }
+        return toValidate;
+    }
+
     public void validate_componentName(String componentName){
-        if(componentName.isEmpty()){ throw new IllegalArgumentException("Component name cannot be empty"); }
-        this.componentName = componentName;
+        if(componentName.isEmpty()){ throw new IllegalArgumentException("Komponent navn kan ikke være tomt"); }
+        this.componentName = replaceComma(componentName);
     }
 
     public void validate_componentCategory(String componentCategory){
-        if(componentCategory.isEmpty()||componentCategory.equals("All")){ throw new IllegalArgumentException("Component category cannot be empty"); }
+        if(componentCategory.isEmpty() || componentCategory.equals("All")){
+            throw new IllegalArgumentException("Komponent kategori kan ikke være tomt"); }
         this.componentCategory = componentCategory;
     }
 
     public void validate_componentSpecs(String componentSpecs){
-        if(componentSpecs.isEmpty()){ throw new IllegalArgumentException("Specifications cannot be empty"); }
-        this.componentSpecs = componentSpecs;
+        if(componentSpecs.isEmpty()){ throw new IllegalArgumentException("Spesifikasjoner kan ikke være tomt"); }
+        this.componentSpecs = replaceComma(componentSpecs);
     }
 
     public void validate_componentNumber(String txtComponentNumber){
         try {
             this.componentNumber = Integer.parseInt(txtComponentNumber);
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid Component Number Format"); }
+            throw new IllegalArgumentException("Ugyldig komponent nummer format"); }
     }
 
     public void validate_componentPrice(String txtComponentPrice){
@@ -36,17 +45,17 @@ public class Validations {
             if(txtComponentPrice.contains(",")){
                 String strPrice = txtComponentPrice.replace(',','.');
                 double componentPrice = Double.parseDouble(strPrice);
-                if(componentPrice <= 0) { throw new InvalidItemDataException("Price must be greater than 0."); }
+                if(componentPrice <= 0) { throw new InvalidItemDataException("Prisen må være større enn 0."); }
                 this.componentPrice = componentPrice;
             } else {
                 double componentPrice = Double.parseDouble(txtComponentPrice);
-                if(componentPrice <= 0) { throw new InvalidItemDataException("Price must be greater than 0."); }
+                if(componentPrice <= 0) { throw new InvalidItemDataException("Prisen må være større enn 0."); }
                 this.componentPrice = componentPrice;
             }
         } catch (InvalidItemDataException e) {
             throw new IllegalArgumentException(e.getMessage());
         } catch (IllegalArgumentException e) {
-            throw new IllegalArgumentException("Invalid Component Price Format");
+            throw new IllegalArgumentException("Ugyldig komponent pris Format");
         }
     }
 
