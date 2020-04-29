473bec7fdf2ba47e974e5d2c11df5e9dc0e8a39a
NimaAB <nimabewrani98@gmail.com>
2020-04-29 13:52:42 +0200

    Login Side er ferdig

---
+++ src/app/User.java
@@ -0,0 +1,14 @@
+package app;
+
+public class User {
+    private String userName;
+    private String password;
+
+    public User(String userName, String password) {
+        this.userName = userName;
+        this.password = password;
+    }
+
+    public String getUserName() { return userName; }
+    public String getPassword() { return password;}
+}
--- src/app/controllers/LoginController.java
+++ src/app/controllers/LoginController.java
@@ -2,24 +2,48 @@ package app.controllers;
 
 import app.Load;
 
+import app.User;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+import validations.Alerts;
 
 public class LoginController {
 
     @FXML BorderPane loginPane;
     @FXML TextField txtUsername, txtPassword;
-    @FXML CheckBox isAdmin;
+    @FXML RadioButton isAdmin;
+    private final User admin = new User("Admin","1234");
+    private final User customer = new User("Custom","1234");
 
     @FXML
     void loggInn(ActionEvent event) {
         Stage stage = (Stage) loginPane.getScene().getWindow();
-        if(isAdmin.isSelected()){
+
+        if(isAdmin()){
             Load.window("views/adminView.fxml","Admin",stage);
-        } else {
-            Load.window("views/customerView.fxml","Customer",stage); }
+        } else if(isCustom()) {
+            Load.window("views/customerView.fxml","Customer",stage);
+        }else{
+            Alerts.warning("Feil Bruker navn eller passord!");
+        }
+    }
+    private boolean isAdmin(){
+        String username = txtUsername.getText();
+        String password = txtPassword.getText();
+        boolean usernameMatches= username.equals(admin.getUserName());
+        boolean passwordMatches= password.equals(admin.getPassword());
+
+        return isAdmin.isSelected() && passwordMatches && usernameMatches;
+    }
+    private boolean isCustom(){
+        String username= txtUsername.getText();
+        String password= txtPassword.getText();
+        boolean usernameMatches= username.equals(customer.getUserName());
+        boolean passwordMatches= password.equals(customer.getPassword());
+
+        return usernameMatches && passwordMatches && !isAdmin.isSelected();
     }
 }
