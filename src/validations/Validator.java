155183018e7457c2189bebbe2c5de07e1d342744
NimaAB <55274614+NimaAB@users.noreply.github.com>
2020-05-11 16:51:07 +0200

    Endre en bit av koden

--- src/validations/Validator.java
+++ src/validations/Validator.java
@@ -33,8 +33,7 @@ public class Validator implements Serializable {
     }
 
     public static void validate_componentCategory(String componentCategory) {
-        boolean isCorrect = Pattern.matches("^[A-ZÆØÅ]?.+$",componentCategory) ||
-                Pattern.matches("[A-ZÆØÅ]?.+$",componentCategory);
+        boolean isCorrect = Pattern.matches("^[A-ZÆØÅ]?.+$",componentCategory) && !componentCategory.contains(",");
         if(!isCorrect){
             throw new IllegalArgumentException("Komponent kategori må være på riktig format og ikke være tomt"); }
         Validator.componentCategory = componentCategory;
