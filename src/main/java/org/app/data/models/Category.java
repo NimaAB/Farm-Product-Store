9eee82c73bf5419dbc3519a38ffc4abc1c34ec19
NimaAB <nimabewrani98@gmail.com>
2021-05-03 23:02:58 +0200

    legget noen metoder i Category class

--- src/main/java/org/app/data/models/Category.java
+++ src/main/java/org/app/data/models/Category.java
@@ -2,12 +2,17 @@ package org.app.data.models;
 
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Category {
+public class Category implements Serializable {
 
-    private final String name;
-    private final ObservableList<String> subCategories = FXCollections.observableArrayList();
+    private String name;
+    private ArrayList<String> subCategories = new ArrayList<>();
 
     public Category(String name){
         this.name = name;
@@ -21,7 +26,37 @@ public class Category {
         return name;
     }
 
-    public ObservableList<String> getSubCategories() {
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public ArrayList<String> getSubCategories() {
         return subCategories;
+    }
+
+    public void setSubCategories(ArrayList<String> list){
+        this.subCategories = list;
+    }
+
+    public void addSubCategory(String newSubCategory){
+        if(!subCategories.contains(newSubCategory)){
+            this.subCategories.add(newSubCategory);
+        }
+        //throw a exception.
+    }
+
+    private void writeObject(ObjectOutputStream ost) throws IOException {
+        ost.defaultWriteObject();
+        ost.writeUTF(getName());
+        ost.writeObject(getSubCategories());
+    }
+
+    private void readObject(ObjectInputStream ist) throws IOException, ClassNotFoundException {
+        String name = ist.readUTF();
+        ArrayList<String> subcategories = (ArrayList<String>) ist.readObject();
+
+        this.setName(name);
+        this.setSubCategories(subcategories);
+
     }
 }
